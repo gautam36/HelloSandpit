a8c9da0a113f3c7477b1896bfa6aa3bd411c11f2
gautam36 <sanjay.thotakura@gmail.com>
2018-01-24 21:01:28 +1000

    Initial

---
+++ helloCalculator/src/main/java/au/com/sanjay/helloCalculator/calculator.java
@@ -0,0 +1,13 @@
+package au.com.sanjay.helloCalculator;
+
+public class calculator implements calculatorInterface {
+
+	public double add(double d1, double d2) {
+		return d1 + d2;
+	}
+
+	public double substract(double d1, double d2) {
+		return d1 - d2;
+	}
+
+}
---
+++ helloCalculator/src/main/java/au/com/sanjay/helloCalculator/calculatorInterface.java
@@ -0,0 +1,8 @@
+package au.com.sanjay.helloCalculator;
+
+public interface calculatorInterface {
+	
+	public double add(double d1, double d2);
+	
+	public double substract(double d1, double d2);
+}
